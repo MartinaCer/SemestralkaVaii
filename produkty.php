cc2f8f9db3d2c02cd13d431b25be04a8a883e7b5
MartinaCer <martinacernekova@azet.sk>
2020-11-08 11:20:28 +0100

    8.11.

--- produkty.php
+++ produkty.php
@@ -19,27 +19,25 @@ if (!isset($_SESSION["meno"])) {
 ?>
 <section class="sectionProdukty">
     <?php
-    for ($i = 1; $i <= 10; $i++) {
-        $selectProdukty = "select * from produkt order by ID";
-        $produkty = mysqli_query($mysqli, $selectProdukty);
-        while ($riadok = $produkty->fetch_array()) {
-            ?>
-            <div class="produktObal">
-                <div class="produktObrazok">
-                    <img alt="<?php echo $riadok["meno"]; ?>" src="obrazky/<?php echo $riadok["obrazok"]; ?>">
-                </div>
-                <div class="produktInfo">
-                    <h2><?php echo $riadok["meno"]; ?></h2>
-                    <h3><?php echo $riadok["cena"] . " €"; ?></h3>
-                    <form method="post">
-                        <input type="hidden" name="id" value="<?php echo $riadok["ID"]; ?>">
-                        <input class="cislo" type="number" name="mnozstvo" value="1" min="1"/>
-                        <input class="button" name="vloz" type="submit" value="Vlož do košíka">
-                    </form>
-                </div>
+    $selectProdukty = "select * from produkt order by ID";
+    $produkty = mysqli_query($mysqli, $selectProdukty);
+    while ($riadok = $produkty->fetch_array()) {
+        ?>
+        <div class="produktObal">
+            <div class="produktObrazok">
+                <img alt="<?php echo $riadok["meno"]; ?>" src="obrazky/<?php echo $riadok["obrazok"]; ?>">
             </div>
-            <?php
-        }
+            <div class="produktInfo">
+                <h2><?php echo $riadok["meno"]; ?></h2>
+                <h3><?php echo $riadok["cena"] . " €"; ?></h3>
+                <form method="post">
+                    <input type="hidden" name="id" value="<?php echo $riadok["ID"]; ?>">
+                    <input class="cislo" type="number" name="mnozstvo" value="1" min="1"/>
+                    <input class="button" name="vloz" type="submit" value="Vlož do košíka">
+                </form>
+            </div>
+        </div>
+        <?php
     }
     ?>
 </section>
